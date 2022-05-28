d6aa37c6cb7617273f6d83e0a9219a1a3f7a16d4
GhaziRiyadh <ghaziriyadh2@gmail.com>
2022-05-28 23:54:30 +0300

    start app

---
+++ src/routes/index.svelte
@@ -0,0 +1,72 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import Counter from "$lib/Counter.svelte";
+	import axios from "axios";
+	let form = new FormData();
+	form.append("username", "root");
+	form.append("password", "root");
+	axios
+		.post("http://127.0.0.1:8000/api/", form)
+		.then((r) => {
+			console.log(r);
+		})
+		.catch((er) => {
+			console.log(er);
+		});
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome bg-gray-500">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
